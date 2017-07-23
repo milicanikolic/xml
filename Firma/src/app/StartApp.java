f3803c62465f374783e06fe4cbd9e78853bd3289
nebojsaSarvan <sarvan.nebojsa@gmail.com>
2017-07-23 13:34:40 +0200

    Rad sa bazom

--- Firma/src/app/StartApp.java
+++ Firma/src/app/StartApp.java
@@ -21,20 +21,23 @@ import com.marklogic.client.DatabaseClientFactory;
 import com.marklogic.client.DatabaseClientFactory.Authentication;
 import com.marklogic.client.document.XMLDocumentManager;
 import com.marklogic.client.io.InputStreamHandle;
+import com.marklogic.client.io.JAXBHandle;
 
 import model.Banka;
 import model.Firma;
+import model.Firme;
 
 @Startup
 @Singleton
 public class StartApp {
-
+	
 	private JAXBContext context;
-
+	Marshaller marshaller;
+	
 	public StartApp() {
-
+		
 	}
-
+	
 	@PostConstruct
 	public void init() {
 	
@@ -60,47 +63,57 @@ public class StartApp {
         MyInterface hello = service.getPort(MyInterface.class);
 
         System.out.println(hello.getHelloWorldAsString("mkyong"));
-        Marshaller marshaller=null;
+        
+        
+
+        String fileName="firma.xml";
+        Firme firme=new Firme();
+        firme.dodajFirmu(new Firma("Firma1", "Novosadskog Sajma 10", "12345", new Banka("Agricole", "123", "500")));
+        firme.dodajFirmu(new Firma("Firma2", "Bulevar Oslobodjenja 55", "56789", new Banka("Generali", "456", "345")));
+       
        try {
-		this.context=JAXBContext.newInstance(Firma.class);
+		this.context=JAXBContext.newInstance(Firme.class);
 		 marshaller=this.context.createMarshaller();
-		 marshaller.marshal(new Firma("Firma1", "Novosadskog Sajma 10", "12345", new Banka("Agricole", "123", "500")), new File("C:/Users/NINA-HOME/Desktop/milica/github/Firma/WebContent/firma.xml"));
+		 marshaller.marshal(firme, new File(System.getProperty("jboss.server.data.dir"),  fileName));
 	} catch (JAXBException e1) {
 		// TODO Auto-generated catch block
 		e1.printStackTrace();
 	}
-       
-       
-       DatabaseClient client =
- 			  DatabaseClientFactory.newClient("localhost", 8003,
- 			                                  "admin", "admin",
- 			                                  Authentication.DIGEST);
 
- 	XMLDocumentManager docMgr = client.newXMLDocumentManager();
- 	
- 	InputStream is=null;
- 	try{
- 		is=new FileInputStream("C:/Users/NINA-HOME/Desktop/milica/github/Firma/WebContent/firma.xml");
- 				is.close();
- 	}
- 	catch(Exception e) {
- 		e.printStackTrace();
- 	}
- 	
- 	InputStreamHandle writeHandle=new InputStreamHandle(is);
- 	docMgr.write("/content/firma.xml", writeHandle);
- 	
- 	
- 	/*StringHandle handleRead = new StringHandle();
- 	docMgr.read("/content/xml/zavrsni_rad.xml", handleRead);
- 	
- 	System.out.println("ispis " + handleRead.get());
- 	
- 	*/
- 	client.release();
+
+        DatabaseClient client =
+        DatabaseClientFactory.newClient("localhost", 8003,
+                                        "admin", "admin",
+                                        Authentication.DIGEST);
+
+        XMLDocumentManager docMgr = client.newXMLDocumentManager();
+     	String fileRead=System.getProperty("jboss.server.data.dir") + "\\" + fileName;
+     	InputStream is=null;
+     	try{
+     		is=new FileInputStream(fileRead);
+     		InputStreamHandle writeHandle=new InputStreamHandle(is);
+     	 	docMgr.write("/content/firma.xml", writeHandle);
+     				is.close();
+     	}
+     	catch(Exception e) {
+     		e.printStackTrace();
+     	}
+     	
+     	
+     	
+     	JAXBHandle<?> jaxHandle=new JAXBHandle<>(context);
+     	docMgr.read("/content/firma.xml", jaxHandle);
+     	Firme firmeBaza=(Firme) jaxHandle.get();
+     	System.out.println("***********************************");
+     	for(Firma f:firmeBaza.getFirme()) {
+     	System.out.println("iz bazeee "+f.getNaziv()+" "+f.getBanka().getNaziv());
+     	
+     
+     	}
+     	
+     	client.release();
+
+
        
-       
-       
- 
 	}
 }
