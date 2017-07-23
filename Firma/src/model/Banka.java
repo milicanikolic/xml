9ce301918defd2cc98b4eb6f7c67d3c416e5870e
ninaManojlovic <ninam94@gmail.com>
2017-07-23 14:04:49 +0200

    Ispravke

--- Firma/src/model/Banka.java
+++ Firma/src/model/Banka.java
@@ -8,17 +8,17 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Banka implements Serializable{
+public class Banka implements Serializable {
 
 	/**
-	 * 
-	 */
+	* 
+	*/
 	private static final long serialVersionUID = 1L;
 
 	private String naziv;
 	private String sifra;
 	private String swiftCode;
-	
+
 	public Banka() {
 		super();
 	}
@@ -54,7 +54,4 @@ public class Banka implements Serializable{
 		this.swiftCode = swiftCode;
 	}
 
-	
-	
-	
 }
--- Firma/src/model/Firma.java
+++ Firma/src/model/Firma.java
@@ -6,25 +6,20 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Firma implements Serializable {
 
 	/**
-	 * 
-	 */
+	* 
+	*/
 	private static final long serialVersionUID = 1L;
-	
+
 	private String naziv;
 	private String adresa;
 	private String PIB;
 	private Banka banka;
-	
-	
-	
-	
-	
+
 	public Firma() {
 		super();
 	}
@@ -36,65 +31,37 @@ public class Firma implements Serializable {
 		PIB = pIB;
 		this.banka = banka;
 	}
-	
+
 	public String getNaziv() {
 		return naziv;
 	}
-
-
-
 
 	public void setNaziv(String naziv) {
 		this.naziv = naziv;
 	}
 
-
-
-
 	public String getAdresa() {
 		return adresa;
 	}
-
-
-
 
 	public void setAdresa(String adresa) {
 		this.adresa = adresa;
 	}
 
-
-
-
 	public String getPIB() {
 		return PIB;
 	}
-
-
-
 
 	public void setPIB(String pIB) {
 		PIB = pIB;
 	}
 
-
-
-
 	public Banka getBanka() {
 		return banka;
 	}
 
-
-
-
 	public void setBanka(Banka banka) {
 		this.banka = banka;
 	}
-
-
-
-
-
-	
-	
 
 }
